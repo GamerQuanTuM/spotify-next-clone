ff527c7b84ab7db7bbc8e4aa76d0eb1e903bdc15
Shuvam <shuvamsantra10@gmail.com>
2023-02-28 14:59:59 +0530

    Fixed Layout for the whole project

--- app/(HomePage)/page.tsx
+++ app/(HomePage)/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react'
-import Header from '../components/Header'
+import Header from '../../components/Header'
 
 export default function Home() {
     const [isScrolled, setIsScrolled] = useState(false);
